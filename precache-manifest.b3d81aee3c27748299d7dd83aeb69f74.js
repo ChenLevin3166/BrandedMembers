21d513bf4fafe8925084a941ff7356be42d20abc
ChenLevin3166 <henc1995@gmail.com>
2020-06-21 00:59:43 +0300

    Updates

---
+++ precache-manifest.b3d81aee3c27748299d7dd83aeb69f74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ada3942ddafab0964fc4f127a1f6b7",
+    "url": "/BrandedMembers/index.html"
+  },
+  {
+    "revision": "59e7564269f488f6759a",
+    "url": "/BrandedMembers/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7ca403d17f4d60c57b73",
+    "url": "/BrandedMembers/static/css/main.a2111251.chunk.css"
+  },
+  {
+    "revision": "59e7564269f488f6759a",
+    "url": "/BrandedMembers/static/js/2.dfa26ba9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BrandedMembers/static/js/2.dfa26ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca403d17f4d60c57b73",
+    "url": "/BrandedMembers/static/js/main.87228b4a.chunk.js"
+  },
+  {
+    "revision": "7022841b6666b9d5ee0e",
+    "url": "/BrandedMembers/static/js/runtime-main.dbe2f857.js"
+  },
+  {
+    "revision": "f4d89c3972e9c7999cca4c6f7d7abea5",
+    "url": "/BrandedMembers/static/media/backImg.f4d89c39.jpg"
+  },
+  {
+    "revision": "bc307f0a110f0e7e12175fc849e37bd7",
+    "url": "/BrandedMembers/static/media/iconCart.bc307f0a.jpg"
+  },
+  {
+    "revision": "5d21153b2953a6c4213bef55ec54af01",
+    "url": "/BrandedMembers/static/media/tent3.5d21153b.jpg"
+  }
+]);
